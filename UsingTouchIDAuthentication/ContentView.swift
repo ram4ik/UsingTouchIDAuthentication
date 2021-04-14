769a44bda1b4929caceed89717043373564a82fa
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-04-14 21:45:56 +0300

    init

--- UsingTouchIDAuthentication/ContentView.swift
+++ UsingTouchIDAuthentication/ContentView.swift
@@ -6,11 +6,33 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            authenticate()
+        }, label: {
+            Text("Login")
+        })
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        let message = "Authenticate"
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: message) { (success, error) in
+                if success {
+                    print("Authentication succeded")
+                } else {
+                    print("FAILED!")
+                }
+            }
+        } else {
+            print("Device has no Touch ID sensor")
+        }
     }
 }
 
