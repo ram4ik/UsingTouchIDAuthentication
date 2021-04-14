943f2ab5f51c960d0e75c9a7fd9cb4e159f11775
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-04-14 21:33:09 +0300

    Initial Commit

---
+++ UsingTouchIDAuthentication/UsingTouchIDAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingTouchIDAuthenticationApp.swift
+//  UsingTouchIDAuthentication
+//
+//  Created by Ramill Ibragimov on 14.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct UsingTouchIDAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
